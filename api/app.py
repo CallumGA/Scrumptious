dfd28092fc42b7cd1fc9ab5b30f697fbb0b712d2
callumanderson <callum.g.anderson@gmail.com>
2023-12-28 15:21:41 -0500

    setup restful api backend with flask using python

---
+++ api/app.py
@@ -0,0 +1,12 @@
+# api/app.py
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello from the Flask backend!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
