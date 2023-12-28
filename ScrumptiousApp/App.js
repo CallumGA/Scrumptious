1ca4a5c134576ab8ba5a53cc82780c04519298bb
callumanderson <callum.g.anderson@gmail.com>
2023-12-28 15:03:30 -0500

    initial setup

---
+++ ScrumptiousApp/App.js
@@ -0,0 +1,10 @@
+// App.js
+
+import React from 'react';
+import HomeScreen from './src/screens/HomeScreen/HomeScreen'; // Import HomeScreen
+
+const App = () => {
+  return <HomeScreen />; // Render HomeScreen
+};
+
+export default App;
---
+++ ScrumptiousApp/src/screens/HomeScreen/HomeScreen.js
@@ -0,0 +1,29 @@
+// src/screens/HomeScreen/HomeScreen.js
+
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome to ScrumptiousApp!</Text>
+      {/* Add more UI elements here as needed */}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  title: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+});
+
+export default HomeScreen;
