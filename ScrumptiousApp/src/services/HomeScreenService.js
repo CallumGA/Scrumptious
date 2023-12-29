9309a57baf48b94cc326c9e11568e880b2c45271
callumanderson <callum.g.anderson@gmail.com>
2023-12-29 14:16:41 -0500

    git ignore update

--- ScrumptiousApp/src/services/HomeScreenService.js
+++ ScrumptiousApp/src/services/HomeScreenService.js
@@ -4,27 +4,6 @@ const BASE_URL = 'http://127.0.0.1:5000'; // URL for iOS simulator
 
 const HomeScreenService = {
 
-  // test ping
-  ping: async () => {
-    try {
-      const response = await fetch(`${BASE_URL}/ping`, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-
-      return await response.json();
-    } catch (error) {
-      console.error('Error during fetch operation:', error);
-      throw error;
-    }
-  },
-
   // get home page recipes
   getRecipes: async () => {
     try {
