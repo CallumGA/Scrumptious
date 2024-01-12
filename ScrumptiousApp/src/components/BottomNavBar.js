2cb4a52f707eb6de41d536f35d1d158675273bf0
callumanderson <callum.g.anderson@gmail.com>
2024-01-12 14:49:24 -0500

    styling search bar

--- ScrumptiousApp/src/components/BottomNavBar.js
+++ ScrumptiousApp/src/components/BottomNavBar.js
@@ -6,9 +6,9 @@ import {
   Image,
   TextInput,
 } from 'react-native';
-import { useNavigation, useRoute } from '@react-navigation/native';
+import {useNavigation, useRoute} from '@react-navigation/native';
 
-const BottomNavBar = ({ onSearchPress, showSearchBar }) => {
+const BottomNavBar = ({onSearchPress, showSearchBar}) => {
   const navigation = useNavigation();
   const route = useRoute();
 
@@ -17,57 +17,61 @@ const BottomNavBar = ({ onSearchPress, showSearchBar }) => {
   };
 
   return (
-      <View style={styles.tabBar}>
-        {showSearchBar ? (
-            <View style={styles.searchBarContainer}>
-              <TextInput
-                  style={styles.input}
-                  placeholder="Search..."
-                  placeholderTextColor="black"
+    <View style={styles.tabBar}>
+      {showSearchBar ? (
+        <View style={styles.searchBarContainer}>
+          <TextInput
+            style={styles.input}
+            placeholder="Search..."
+            placeholderTextColor="black"
+          />
+          <TouchableOpacity
+            style={[styles.iconButton, styles.closeIcon]}
+            onPress={onSearchPress}>
+            <Image
+              source={require('../assets/close.png')}
+              resizeMode="contain"
+              style={styles.icon}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[styles.iconButton, styles.arrowIcon]}
+            onPress={handleArrowPress}>
+            <Image
+              source={require('../assets/arrow.png')}
+              resizeMode="contain"
+              style={styles.icon}
+            />
+          </TouchableOpacity>
+        </View>
+      ) : (
+        <>
+          {route.name === 'Home' && (
+            <TouchableOpacity onPress={onSearchPress}>
+              <Image
+                source={require('../assets/search.png')}
+                resizeMode="contain"
+                style={styles.icon}
               />
-              <TouchableOpacity style={[styles.iconButton, styles.closeIcon]} onPress={onSearchPress}>
-                <Image
-                    source={require('../assets/close.png')}
-                    resizeMode="contain"
-                    style={styles.icon}
-                />
-              </TouchableOpacity>
-              <TouchableOpacity style={[styles.iconButton, styles.arrowIcon]} onPress={handleArrowPress}>
-                <Image
-                    source={require('../assets/arrow.png')}
-                    resizeMode="contain"
-                    style={styles.icon}
-                />
-              </TouchableOpacity>
-            </View>
-        ) : (
-            <>
-              {route.name === 'Home' && (
-                  <TouchableOpacity onPress={onSearchPress}>
-                    <Image
-                        source={require('../assets/search.png')}
-                        resizeMode="contain"
-                        style={styles.icon}
-                    />
-                  </TouchableOpacity>
-              )}
-              <TouchableOpacity onPress={() => navigation.navigate('Home')}>
-                <Image
-                    source={require('../assets/home.png')}
-                    resizeMode="contain"
-                    style={styles.icon}
-                />
-              </TouchableOpacity>
-              <TouchableOpacity onPress={() => navigation.navigate('NewRecipe')}>
-                <Image
-                    source={require('../assets/eat.png')}
-                    resizeMode="contain"
-                    style={styles.icon}
-                />
-              </TouchableOpacity>
-            </>
-        )}
-      </View>
+            </TouchableOpacity>
+          )}
+          <TouchableOpacity onPress={() => navigation.navigate('Home')}>
+            <Image
+              source={require('../assets/home.png')}
+              resizeMode="contain"
+              style={styles.icon}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => navigation.navigate('NewRecipe')}>
+            <Image
+              source={require('../assets/eat.png')}
+              resizeMode="contain"
+              style={styles.icon}
+            />
+          </TouchableOpacity>
+        </>
+      )}
+    </View>
   );
 };
 
